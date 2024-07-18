9cccfc78b02cc6f7b6b5ecd5f3cafa5bdfea149b
Mekala Narendra <narendramekala58@gmail.com>
2024-07-18 12:02:18 +0530

    added new changes

--- send_message.py
+++ send_message.py
@@ -2,8 +2,8 @@ import pywhatkit as kit
 import time
 
 # Define the phone number and message
-phone_number = "+917732070952"  # Replace with the recipient's phone number in international format
-message = "nuvvo bokka ra"
+phone_number = input("enter your number : ")
+message = input("enter the message : ")
 
 # Specify the time at which the message should be sent (24-hour format)
 hour = 12  # Replace with the desired hour
@@ -11,9 +11,11 @@ minute = 30  # Replace with the desired minute
 
 # Send the WhatsApp message
 # kit.sendwhatmsg(phone_number, message, hour, minute)
-while True:
-    # Send the WhatsApp message immediately
-    kit.sendwhatmsg_instantly(phone_number, message)
+kit.sendwhatmsg_instantly(phone_number, message)
 
-    # Wait for a specified amount of time before sending the next message
-    time.sleep(2)  # Send a message every 60 seconds
+# while True:
+#     # Send the WhatsApp message immediately
+#     kit.sendwhatmsg_instantly(phone_number, message)
+
+#     # Wait for a specified amount of time before sending the next message
+#     time.sleep(2)  # Send a message every 60 seconds
